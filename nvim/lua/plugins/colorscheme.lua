a017c7348702338e2319dd78a9a14011d5407913
Koxepis <andiasar11@gmail.com>
2024-05-26 07:16:43 +0700

    fix: make it lazy loaded and readjust some config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,12 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
+  -- other plugins...
   {
     "catppuccin/nvim",
-    lazy = true,
     name = "catppuccin",
-    opts = {
+    lazy = false, -- make sure to set this to false for the setup to work
+    config = function()
       require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        flavour = "auto", -- latte, frappe, macchiato, mocha
         background = { -- :h background
           light = "latte",
           dark = "mocha",
@@ -20,7 +15,7 @@ return {
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-          enabled = true, -- dims the background color of inactive window
+          enabled = false, -- dims the background color of inactive window
           shade = "light",
           percentage = 0.15, -- percentage of the shade to apply to the inactive window
         },
@@ -46,46 +41,50 @@ return {
         custom_highlights = {},
         default_integrations = true,
         integrations = {
+          cmp = true,
+          gitsigns = true,
           nvimtree = true,
+          treesitter = true,
+          notify = true,
           mini = {
             enabled = true,
             indentscope_color = "",
-            aerial = true,
-            alpha = true,
-            cmp = true,
-            dashboard = true,
-            flash = true,
-            gitsigns = true,
-            headlines = true,
-            illuminate = true,
-            indent_blankline = { enabled = true },
-            leap = true,
-            lsp_trouble = true,
-            mason = true,
-            markdown = true,
-            native_lsp = {
-              enabled = true,
-              underlines = {
-                errors = { "undercurl" },
-                hints = { "undercurl" },
-                warnings = { "undercurl" },
-                information = { "undercurl" },
-              },
-            },
-            navic = { enabled = true, custom_bg = "lualine" },
-            neotest = true,
-            neotree = true,
-            noice = true,
-            notify = true,
-            semantic_tokens = true,
-            telescope = true,
-            treesitter = true,
-            treesitter_context = true,
-            which_key = true,
           },
+          aerial = true,
+          alpha = true,
+          dashboard = true,
+          flash = true,
+          headlines = true,
+          illuminate = true,
+          indent_blankline = { enabled = true },
+          leap = true,
+          lsp_trouble = true,
+          mason = true,
+          markdown = true,
+          native_lsp = {
+            enabled = true,
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+            },
+          },
+          navic = { enabled = true, custom_bg = "lualine" },
+          neotest = true,
+          neotree = true,
+          noice = true,
+          semantic_tokens = true,
+          telescope = true,
+          treesitter_context = true,
+          which_key = true,
           -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
-      }),
-    },
+      })
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
+  -- other plugins...
 }
