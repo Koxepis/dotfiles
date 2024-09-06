c9a234fa952ec84d7dbfcec5d51b73cb2521d6ee
Koxepis <andiasar11@gmail.com>
2024-09-06 22:05:43 +0700

    adjust config

--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -10,26 +10,54 @@ return {
     },
     { "<leader>uN", "<cmd>Notifications<cr>", desc = "Show Notifications" },
   },
-  opts = {
-    render = "default",
-    stages = "slide",
-    background_colour = "#cba6f7",
-    timeout = 1500,
-    max_height = function()
-      return math.floor(vim.o.lines * 0.75)
-    end,
-    max_width = function()
-      return math.floor(vim.o.columns * 0.75)
-    end,
-    on_open = function(win)
-      vim.api.nvim_win_set_config(win, { zindex = 100 })
-    end,
-  },
-  init = function()
-    -- when noice is not enabled, install notify on VeryLazy
+  config = function()
+    local notify = require("notify")
+
+    notify.setup({
+      render = "default",
+      stages = "slide",
+      level = vim.log.levels.INFO,
+      background_colour = "#21222c",
+      icons = {
+        ERROR = "",
+        WARN = "⚠",
+        INFO = "ℹ",
+        DEBUG = "",
+        TRACE = "✎",
+      },
+      time_formats = {
+        notification = "%H:%M",
+        notification_history = "%FT%T",
+      },
+      on_close = nil,
+      minimum_width = 50,
+      fps = 60,
+      top_down = true,
+      highlights = {
+        background = "Normal",
+        title = "Title",
+        border = "FloatBorder",
+        icon = "Special",
+      },
+      timeout = 1500,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    })
+
+    -- Set vim.notify to use nvim-notify
+    vim.notify = notify
+
+    -- Handle the case when noice is not enabled
     if not LazyVim.has("noice.nvim") then
       LazyVim.on_very_lazy(function()
-        vim.notify = require("notify")
+        vim.notify = notify
       end)
     end
   end,
