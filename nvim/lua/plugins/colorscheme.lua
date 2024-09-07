955d257c37fa7528c48f299020bbb6192d289eba
Koxepis <andiasar11@gmail.com>
2024-09-07 07:38:42 +0700

    fix eldritch

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,93 +1,12 @@
 return {
-  -- other plugins...
+  --init here
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false, -- make sure to set this to false for the setup to work
-    config = function()
-      require("catppuccin").setup({
-        flavour = "auto", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-          light = "latte",
-          dark = "mocha",
-        },
-        transparent_background = true, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
-          shade = "dark",
-          percentage = 0.15, -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false, -- Force no italic
-        no_bold = false, -- Force no bold
-        no_underline = false, -- Force no underline
-        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" }, -- Change the style of comments
-          conditionals = { "italic" },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = { "italic" },
-          operators = {},
-          -- miscs = {}, -- Uncomment to turn off hard-coded styles
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        default_integrations = true,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = false,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
-          aerial = true,
-          alpha = true,
-          dashboard = true,
-          flash = true,
-          headlines = true,
-          illuminate = true,
-          indent_blankline = { enabled = true },
-          leap = true,
-          lsp_trouble = true,
-          mason = true,
-          markdown = true,
-          native_lsp = {
-            enabled = true,
-            underlines = {
-              errors = { "undercurl" },
-              hints = { "undercurl" },
-              warnings = { "undercurl" },
-              information = { "undercurl" },
-            },
-          },
-          navic = { enabled = true, custom_bg = "lualine" },
-          neotest = true,
-          neotree = true,
-          noice = true,
-          semantic_tokens = true,
-          telescope = true,
-          treesitter_context = true,
-          which_key = true,
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        },
-      })
-
-      -- setup must be called before loading
-      vim.cmd.colorscheme("catppuccin")
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "eldritch",
+    },
   },
-  -- other plugins...
-
+  -- tokyonight
   {
     "folke/tokyonight.nvim",
     lazy = true,
@@ -100,19 +19,26 @@ return {
       },
     },
   },
-
+  -- eldritch
   {
     "eldritch-theme/eldritch.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
-  },
-
-  {
-    "eldritch-theme/eldritch.nvim",
-    lazy = true,
     name = "eldritch",
     opts = {
+      lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true, fg = "#646573" }, -- Added fg color here
+        keywords = { italic = true },
+        string = { italic = true, fg = "#fefff2" },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
       -- This function is found in the documentation
       on_highlights = function(highlights)
         -- nvim-spectre highlight colors
